a3b74ac42228ae1ecc42a707340ef3aefa39ba04
nikhil <nikhilpokuri143@gmail.com>
2024-06-25 21:35:30 +0530

    updated

--- practice.py
+++ practice.py
@@ -1,29 +1,53 @@
+"""
+Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+For example, the square matrix  is shown below:
 
-votes = [
-       'Tony', 'Bucky', 'Rogers', 'Damon', 'Stefan',
-       'Bucky', 'Rogers', 'Rogers', 'Tony', 'Damon',
-      'Elena', 'Stefan', 'Tony', 'Thanos', 'Elena',
-       'Thanos', 'Matt', 'Rose', 'Thanos', 'Damon'
-]
-age = {
-    'Tony': 61,
-    'Bucky': 54,
-    'Stefan': 79,
-    'Rogers': 80,
-    'Vicky': 34,
-    'Thanos': 50,
-    'Matt': 55,
-    'Rose': 42,
-    'Elena': 70,
-    'Damon': 78
-}
+Input Format:
+    - The first line contains a single integer n,
+       the number of rows and columns in the square matrix arr.
+    - next n lines contains n-space separated integers  
 
-emails = [
-    'john.deer@gmail.com',
-    'alex.barner@yahoo.com',
-    'brad.cooper@hotmail.com',
-    'cindy.barner@hotmail.com',
-    'matt.damon@gmail.com',
-    'george.cloony@yahoo.com',
-    'mc.barner@hotmail.com'
-]
+Output Format:
+    - Return the absolute diff b/w the sums of matrix's two diagonals as a single integer.
+
+Sample Input
+3
+1 2 3
+4 5 6
+9 8 9 
+
+Sample Output
+2
+
+Explanation:
+The left-to-right diagonal (1+5+9= 15). The right to left diagonal (3+5+9 = 17). 
+Their absolute difference is |15-17|=2.
+
+"""
+# # input
+# n = int(input("n:"))
+# matrix = []
+
+# for _ in range(n):
+#     row = list(map(int, input().split()))
+#     matrix.append(row)
+
+n = int(input("n:"))
+matrix = [list(map(int, input().split())) for _ in range(n)]
+print(matrix)
+for i in matrix:
+    print(i)
+# logic
+left_digonal = 0
+right_digonal = 0
+
+for row in range(n):
+    for col in range(n):
+        if row == col:
+            left_digonal += matrix[row][col]
+
+        if row + col == n - 1:
+            right_digonal += matrix[row][col]
+        
+#output
+print(abs(left_digonal - right_digonal))
