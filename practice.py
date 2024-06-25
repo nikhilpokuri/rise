724920a982dc587c10656f14875241f48e2fa3f7
nikhil <nikhilpokuri143@gmail.com>
2024-06-25 10:29:40 +0530

    vote data

--- practice.py
+++ practice.py
@@ -1,8 +1,19 @@
-"""
-- create empty dictionary
-- put keys from 1 to 10
-- values as True
 
-OUTPUT:
-{1:True, 2:True, ... 10:True}
-"""
+votes = [
+       'Tony', 'Bucky', 'Rogers', 'Damon', 'Stefan',
+       'Bucky', 'Rogers', 'Rogers', 'Tony', 'Damon',
+      ';Elena', 'Stefan', 'Tony', 'Thanos', 'Elena',
+       'Thanos', 'Matt', 'Rose', 'Thanos', 'Damon'
+]
+age = {
+    'Tony': 61,
+    'Bucky': 54,
+    'Stefan': 79,
+    'Rogers': 80,
+    'Vicky': 34,
+    'Thanos': 50,
+    'Matt': 55,
+    'Rose': 42,
+    'Elena': 70,
+    'Damon': 78
+}
